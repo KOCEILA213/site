0d2c7254ea6b9de47cedf04235c7391463ebb503
KOCEILA213 <145122020+KOCEILA213@users.noreply.github.com>
2023-11-08 15:59:45 +0100

    Add files via upload

---
+++ JavaScript/tp2_js/tp2_js.js
@@ -0,0 +1,22 @@
+function moyenne(){
+    var note1 = document.getElementById("n1").value;
+    var note2 = document.getElementById("n2").value;
+    var note3 = document.getElementById("n3").value;
+    var coeff1 = document.getElementById("c1").value;
+    var coeff2 = document.getElementById("c2").value;
+    var coeff3 = document.getElementById("c3").value;
+    var moyenne = (Number(note1)*Number(coeff1)+Number(note2)*Number(coeff2)+Number(note3)*Number(coeff3))/(Number(coeff1)+Number(coeff2)+Number(coeff3))
+    moy1.value=moyenne
+
+
+    if (moyenne<10)
+        ment1.value="Redoublant";
+    else if (moyenne<14)
+        mention1.value="Admis";
+    else if (moyenne<16)
+        ment1.value="Admis, Mention : Bien";
+    else if (moyenne<=20)
+        ment1.value="Admis, Mention : Très bien";
+    else 
+        ment1.value="Erreur"
+}
